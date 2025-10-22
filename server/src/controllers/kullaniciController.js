fe7675a8d0430155886bf376015514524e1586f9
mertcaneren0 <58989361+mertcaneren0@users.noreply.github.com>
2025-10-22 03:28:55 +0300

    live

---
+++ server/src/controllers/kullaniciController.js
@@ -0,0 +1,37 @@
+const getAllKullanicilar = async (req, res) => {
+  return res.status(501).json({ message: 'Not Implemented: getAllKullanicilar' });
+};
+
+const createKullanici = async (req, res) => {
+  return res.status(501).json({ message: 'Not Implemented: createKullanici' });
+};
+
+const getKullaniciById = async (req, res) => {
+  return res.status(501).json({ message: 'Not Implemented: getKullaniciById' });
+};
+
+const updateKullanici = async (req, res) => {
+  return res.status(501).json({ message: 'Not Implemented: updateKullanici' });
+};
+
+const deleteKullanici = async (req, res) => {
+  return res.status(501).json({ message: 'Not Implemented: deleteKullanici' });
+};
+
+const updateProfile = async (req, res) => {
+  return res.status(501).json({ message: 'Not Implemented: updateProfile' });
+};
+
+const updatePassword = async (req, res) => {
+  return res.status(501).json({ message: 'Not Implemented: updatePassword' });
+};
+
+module.exports = {
+  getAllKullanicilar,
+  createKullanici,
+  getKullaniciById,
+  updateKullanici,
+  deleteKullanici,
+  updateProfile,
+  updatePassword,
+};
